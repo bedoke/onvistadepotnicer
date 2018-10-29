a6b44ac9d53f53d96c773b245deb255ee532d426
kedobe <kevin.berg@yahoo.de>
2018-10-29 20:32:07 +0100

    inital push

---
+++ nicerscript.js
@@ -0,0 +1,5 @@
+// Replace all images by cats
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
